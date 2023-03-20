768040890cf7b4c35fb567e0d32c4c81206f6ab5
quantumish <freifeld.david@gmail.com>
2023-03-19 20:01:17 -0700

    Incredibly hacky and bad first stab at tiling

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ PREFIX?=/usr/X11R6
 CFLAGS?=-Os -pedantic -Wall
 
 all:
-	$(CC) $(CFLAGS) -I$(PREFIX)/include tinywm.c -L$(PREFIX)/lib -lX11 -o tinywm
+	$(CC) $(CFLAGS) -I$(PREFIX)/include stewm.c -L$(PREFIX)/lib -lX11 -o stewm
 
 clean:
 	rm -f tinywm
--- stewm.c
+++ stewm.c
@@ -18,10 +18,37 @@ int main(void)
     XGrabButton(dpy, 3, Mod1Mask, DefaultRootWindow(dpy), True,
             ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 
+    /* XSelectInput(dpy, DefaultRootWindow(dpy), SubstructureRedirectMask | SubstructureNotifyMask | ResizeRedirectMask); */
+
     start.subwindow = None;
+
     for(;;)
     {
-        XNextEvent(dpy, &ev);
+      	XNextEvent(dpy, &ev);
+
+	Window root, parent;
+	Window *children;
+	unsigned int nchildren;
+
+	int snum = DefaultScreen(dpy);
+	XQueryTree(dpy, DefaultRootWindow(dpy), &root, &parent, &children, &nchildren);
+	int num_viewable = 0;
+	for (int i = 0; i < nchildren; i++) {
+	    XGetWindowAttributes(dpy, children[i], &attr);
+	    if (attr.map_state == IsViewable) num_viewable += 1;        	    
+	}
+	for (int i = 0; i < nchildren; i++) {
+	    XGetWindowAttributes(dpy, children[i], &attr);
+	    if (attr.map_state == IsViewable) {
+		XMoveResizeWindow(dpy, children[i], i*DisplayWidth(dpy, snum)/num_viewable, 0, DisplayWidth(dpy, snum)/num_viewable, DisplayHeight(dpy, snum));
+	    }
+	}
+
+	/* if(ev.type == CreateNotify) { */
+	/*     int snum = DefaultScreen(dpy); */
+	/*     XGetWindowAttributes(dpy, ev.xcreatewindow.window, &attr); */
+        /*     XMoveResizeWindow(dpy, ev.xcreatewindow.window, 500, 500, 100, 100); */
+	/* } */
         if(ev.type == KeyPress && ev.xkey.subwindow != None)
             XRaiseWindow(dpy, ev.xkey.subwindow);
         else if(ev.type == ButtonPress && ev.xbutton.subwindow != None)
@@ -39,8 +66,9 @@ int main(void)
                 MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
                 MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
         }
-        else if(ev.type == ButtonRelease)
+        else if(ev.type == ButtonRelease) {
             start.subwindow = None;
+	}
     }
 }
 
